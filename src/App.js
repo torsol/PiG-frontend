8b1315441cd25d5b562512ff7135d8dca9473d2e
Torstein Solberg <soltors@gmail.com>
2020-09-28 14:35:05 +0200

    cleaned up some code

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
-import React, {useState, useEffect} from 'react';
-import Sidebar from './Sidebar';
-import Map from './Map'
-import {getRandomColor} from "./utils/RandomColor"
+import React, { useState, useEffect } from "react";
+import Sidebar from "./Sidebar";
+import Map from "./Map";
+import { getRandomColor } from "./utils/RandomColor";
 
 const App = (props) => {
+  const [layers, setLayers] = useState([]);
 
-    const [layers, setLayers] = useState([])
+  const addLayerToState = (layers, setLayers) => {
+    return (newValue, operation) => {
+      newValue["name"] = operation + "_" + Math.random().toString(36).slice(2); //generates random name for the layer
+      newValue["color"] = getRandomColor(); //generates random color
+      setLayers([...layers, newValue]);
+    };
+  };
 
-    const addLayerToState = (layers, setLayers) => {
-        return (newValue, operation) => {
-            newValue["name"] = operation + "_" + Math.random().toString(36).slice(2) //generates random name for the layer
-            newValue["color"] = getRandomColor() //generates random color
-            setLayers([...layers, newValue])
-        }
-    } 
+  useEffect(() => {
+    console.log("App", "State updated");
+  }, [layers]);
 
-    useEffect(() => {
-        console.log('App', 'State updated')
-      }, [layers]);
-
-    return (
-        <div>
-            <Sidebar addLayerToState={addLayerToState(layers, setLayers)} layers={layers}/>
-            <Map layers ={layers} />
-        </div>
-    )
-
+  return (
+    <div>
+      <Sidebar
+        addLayerToState={addLayerToState(layers, setLayers)}
+        layers={layers}
+      />
+      <Map layers={layers} />
+    </div>
+  );
 };
 
-export default App
+export default App;
