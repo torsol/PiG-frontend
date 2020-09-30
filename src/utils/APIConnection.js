32219c37d263d94ae1fbfdf466af5fb804e5ecd8
Torstein Solberg <soltors@gmail.com>
2020-09-30 15:05:20 +0200

    refactored to backend

--- src/utils/APIConnection.js
+++ src/utils/APIConnection.js
@@ -10,11 +10,11 @@ function cleanResponse(response) {
 function calculateBuffer(addLayerToState, inputData) {
   if (inputData[0]){
     let processedInput = {}
-    processedInput['layers'] = inputData[0]
+    processedInput = inputData[0]
     processedInput['value'] = 10
     inputData = processedInput
   }
-  console.log()
+
   return function () {
     axios
       .post("http://localhost:5000/api/buffer", inputData)
