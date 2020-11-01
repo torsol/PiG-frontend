e887340329e377386ce6d1b1e2cc1a7f87521059
Torstein Solberg <soltors@gmail.com>
2020-11-01 12:25:30 +0100

    Updates

---
+++ precache-manifest.a0be3dd9e06fc894305e6ea63019347d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "482a4bfd1ae54464b7baff1a2a055b9e",
+    "url": "/PiG-frontend/index.html"
+  },
+  {
+    "revision": "4260a0eca7a0d47610fb",
+    "url": "/PiG-frontend/static/css/2.945747aa.chunk.css"
+  },
+  {
+    "revision": "4260a0eca7a0d47610fb",
+    "url": "/PiG-frontend/static/js/2.11066a07.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/PiG-frontend/static/js/2.11066a07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "039bed5ec8f4847ef2f6",
+    "url": "/PiG-frontend/static/js/main.772b5719.chunk.js"
+  },
+  {
+    "revision": "5bf79ab3c5c70b9a465e",
+    "url": "/PiG-frontend/static/js/runtime-main.747a819b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3f4398f3f4ddcbd63003f16fac795c21.js"
+  "/PiG-frontend/precache-manifest.a0be3dd9e06fc894305e6ea63019347d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PiG-frontend/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
